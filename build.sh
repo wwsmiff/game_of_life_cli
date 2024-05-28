660a704619e09b1fa4bfcb137d422f2af39d9ae3
wwsmiff <arnav0872@gmail.com>
2024-05-28 16:33:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ CFLAGS="-O2 -Wall -Wextra -std=c17"
 LFLAGS=""
 EXE="game_of_life"
 SRCDIR="./src"
-INCLUDEDIR="./include"
 BUILDDIR="./build"
 
-$CC -I$INCLUDEDIR $CFLAGS $SRCDIR/*.c -o $BUILDDIR/$EXE $LFLAGS
+$CC $CFLAGS $SRCDIR/*.c -o $BUILDDIR/$EXE $LFLAGS
